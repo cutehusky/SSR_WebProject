8a527645f881354ba5c68673f37a51f8b3be5e67
MH221B <22127131@student.hcmus.edu.vn>
2024-12-07 15:35:09 +0700

    finish writer view articles

--- src/Controllers/WriterController.ts
+++ src/Controllers/WriterController.ts
@@ -47,8 +47,39 @@ export class WriterController {
   // /writer/myArticles?state=
   getMyArticleList(req: Request, res: Response) {
     const state = (req.query.state as string) || "all";
-    res.render("MyArticleView");
+    const articles = [
+      {
+        title: "Lorem ipsum",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aperiam illo omnis quas, aut facilis error. Enim numquam explicabo sed distinctio natus hic illum molestias assumenda, ducimus excepturi architecto beatae?",
+        date: "01/01/2024",
+        category: "Lorem, ipsum",
+        subcategory: "Lorem, ipsum",
+        cover: '/logo.jpg'
+      },
+      {
+        title: "Lorem ipsum",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aperiam illo omnis quas, aut facilis error. Enim numquam explicabo sed distinctio natus hic illum molestias assumenda, ducimus excepturi architecto beatae?",
+        date: "01/01/2024",
+        category: "Lorem, ipsum",
+        subcategory: "Lorem, ipsum",
+        cover: '/logo.jpg'
+      },
+      {
+        title: "Lorem ipsum",
+        abstract: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aperiam illo omnis quas, aut facilis error. Enim numquam explicabo sed distinctio natus hic illum molestias assumenda, ducimus excepturi architecto beatae?",
+        date: "01/01/2024",
+        category: "Lorem, ipsum",
+        subcategory: "Lorem, ipsum",
+        cover: '/logo.jpg'
+      }
+    ] 
+    res.render("Writer/WriterViewArticles", {
+      customCss: ["Writer.css"],
+      state,
+      articles
+    });
   }
+
 
   getWriterHome(req: Request, res: Response) {
     res.render("Writer/WriterHome", { 
