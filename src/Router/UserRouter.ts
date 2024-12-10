f2885cc3d26ba514097a3ec60a166ab4fbe8e23c
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2024-12-10 22:28:36 +0700

    Small fixes UI for Admin & User

--- src/Router/UserRouter.ts
+++ src/Router/UserRouter.ts
@@ -1,13 +1,12 @@
-import express from "express";
-import {UserController} from "../Controllers/UserController";
-
+import express from 'express';
+import { UserController } from '../Controllers/UserController';
 
 const router = express.Router();
 
 const userController = new UserController();
 
 router.get('/forgot-password', userController.forgotPassword);
-router.get('/profile', userController.getUserProfile);
+router.get('/profile/:id', userController.getUserProfile);
 
 router.post('/login', userController.logIn);
 router.post('/register', userController.register);
@@ -15,4 +14,4 @@ router.post('/send-otp', userController.sendOTP);
 router.post('/reset-password-by-otp', userController.resetPasswordByOTP);
 router.post('/reset-password', userController.resetPassword);
 
-export {router as UserRouter}
+export { router as UserRouter };
