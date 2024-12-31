a3dbe3164ced3aca2f4c0f4f2812079000654d98
thaihuyentung04@gmail.com <thaihuyentung04@gmail.com>
2024-12-31 16:59:53 +0700

    Google SignIn (deploy url)

--- src/Utils/Auth.ts
+++ src/Utils/Auth.ts
@@ -13,7 +13,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID || '',
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-            callbackURL: 'http://localhost:3000/auth/google/callback',
+            callbackURL:
+                process.env.GOOGLE_CALLBACK_URL ||
+                'http://localhost:3000/auth/google/callback',
             passReqToCallback: true,
         },
         function (
